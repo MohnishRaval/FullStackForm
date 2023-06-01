ac454c4b1ba7a0b59f79a63d0abd1db362114bc4
Mohnish Raval <mohnishraval40@gmail.com>
2023-06-01 00:03:19 -0400

    Automated script

---
+++ update_repo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Update submodule 1
+cd frontend
+git checkout main
+git pull origin main
+cd ..
+
+# Update submodule 2
+cd backend
+git checkout main
+git pull origin main
+cd ..
+
+# Commit the submodule updates in the parent repository
+git add frontend backend
+git commit -m "Update frontend backend modules"
+
+# Push the changes to the remote repository
+git push origin main
