9bf8cdce1aa47e617cf4b799199faa99d001a0b8
Mohnish Raval <mohnishraval40@gmail.com>
2023-06-08 16:36:09 -0400

    changes

--- update_repo.sh
+++ update_repo.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Start the SSH agent
+eval "$(ssh-agent -s)"
+
+# Add your SSH key to the agent
+ssh-add /c/Users/mohni/.ssh/id_ed25519
+
 # Update submodule 1
 cd frontend
 git checkout main
@@ -18,3 +24,6 @@ git commit -m "Update frontend backend modules"
 
 # Push the changes to the remote repository
 git push origin main
+
+# Stop the SSH agent
+ssh-agent -k
